bbd9ace36d9744092044e9a6f08112e50aacff12
frosquid <frosquid@hotmail.com>
2022-11-03 05:25:45 +0700

    updated

--- js/events/handlers/touchmovehandler.js
+++ js/events/handlers/touchmovehandler.js
@@ -14,12 +14,12 @@ export default function touchmove(ev,obj){
             const [radius,degrees] = RadiusDegrees.get(touchCaches[idx].joyStick.outter.x,touchCaches[idx].joyStick.outter.y,toucheX,toucheY);
             const [x,y] = Cordinate.get(touchCaches[idx].joyStick.outter.x,touchCaches[idx].joyStick.outter.y,touchCaches[idx].joyStick.maxOffsetRadius,degrees);
             if(touches[i].indentifier === touchCaches[idx].joyStick.indentifier && radius <= touchCaches[idx].joyStick.maxOffsetRadius){
-                touchCaches[idx].joyStick.inner.x = toucheX;
-                touchCaches[idx].joyStick.inner.y = toucheY;
+                touchCaches[i].joyStick.inner.x = toucheX;
+                touchCaches[i].joyStick.inner.y = toucheY;
             }
             if(touches[i].indentifier === touchCaches[idx].joyStick.indentifier && radius >= touchCaches[idx].joyStick.maxOffsetRadius){
-                touchCaches[idx].joyStick.inner.x = x;
-                touchCaches[idx].joyStick.inner.y = y;
+                touchCaches[i].joyStick.inner.x = x;
+                touchCaches[i].joyStick.inner.y = y;
             }
             
             console.log(touchCaches)
